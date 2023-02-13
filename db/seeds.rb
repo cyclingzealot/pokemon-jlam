599030655f1db5fe8ce18198d15e09332a461319
Julien Lamarche <julien.lamarche@gmail.com>
2023-02-12 21:17:33 -0500

    added db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Creature.count > 0
+  warn "Looks like the database is already seeded"
+  exit 0
+end
+
+file_path = File.join(Rails.root, "data", "pokemon.csv")
+raise "File #{file_path} does not exist" unless File.exist?(file_path)
+
+Creature.update_from_csv(file_path, nil, "#", { noPrompt: true, dontUpdate: true })
