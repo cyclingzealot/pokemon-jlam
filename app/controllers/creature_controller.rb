c3ed27088121c66ae8dce83b02e90c7621889d43
Julien Lamarche <julien.lamarche@gmail.com>
2023-02-12 22:38:06 -0500

    update test working

--- app/controllers/creature_controller.rb
+++ app/controllers/creature_controller.rb
@@ -5,16 +5,22 @@ class CreatureController < ApplicationController
 
   def show
     respond_to do |format|
-      format.json { redner json: @creature.attributes }
+      format.json { render json: @creature.attributes }
     end
   end
 
-  def update; end
+  def update
+    respond_to do |format|
+      if @creature.update(creature_params)
+        format.json { render json: @creature.attributes, status: :ok }
+      else
+        format.json { render json: @creature.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def create
-    return unless @create.save
-
-    status :no_content
+    status (@create.save ? :no_content : :unprocessable_entity)
   end
 
   def destroy
@@ -29,5 +35,9 @@ class CreatureController < ApplicationController
 
   def set_creature
     @creature = Creature.find(params[:id])
+  end
+
+  def creature_params
+    params.require(:creature).permit(:name, :type1, :type2, :total, :hp, :attack, :defense, :special_attack, :special_defense, :speed, :generation, :legendary)
   end
 end
--- test/controllers/creature_controller_test.rb
+++ test/controllers/creature_controller_test.rb
@@ -27,4 +27,27 @@ class CreatureControllerTest < ActionDispatch::IntegrationTest
     flunk("Test not done yet")
     assert_response :ok
   end
+
+  test "it can update a creature" do
+    c = creatures(:one)
+    update_params = {
+      name: "Sutcliffe",
+      type1: "Water",
+      type2: "Normal",
+      total: 500,
+      hp: 90,
+      attack: 90,
+      defense: 100,
+      special_attack: 95,
+      special_defense: 70,
+      speed: 10,
+      generation: 7,
+      legendary: false,
+    }.map { |k, v| [k.to_s, v] }.to_h
+
+    patch creature_path(c), params: { creature: update_params }, as: :json
+
+    assert_equal update_params, c.reload.attributes.slice(*(update_params.keys.map(&:to_s)))
+    assert_response :ok
+  end
 end
